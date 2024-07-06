1dcff8d80770f2f9b2bc3e44f1671f3f9d039d32
yonainthefish <yonaisgood@gmail.com>
2024-07-07 00:52:12 +0900

    feat : 게시물 삭제를 위한 액션과 리듀서 추가, ToDoCard 컴포넌트에 삭제 기능 추가

--- week2/src/components/toDoCard/ToDoCard.jsx
+++ week2/src/components/toDoCard/ToDoCard.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import Button from '../common/button/Button';
-
+// import {deleteTodo} from "../"
 import * as Styled from './ToDoCardStyeld';
+import { useDispatch } from 'react-redux';
+import { deleteToDo } from '../../modules/todos';
 
-export default function ToDoCard() {
+export default function ToDoCard(props) {
+  const { toDo } = props;
+  const dispatch = useDispatch();
+
+  if (!toDo) {
+    return null;
+  }
+
+  const onDeleteToDo = (id) => {
+    dispatch(deleteToDo(id));
+  };
+
   return (
-    <Styled.TodoCardBox>
+    <Styled.TodoCardBox key={toDo.id}>
       <Button width="100px">Detail</Button>
-      <h3>제목</h3>
-      <p>내용</p>
+      <h3>{toDo.title}</h3>
+      <p>{toDo.contents}</p>
       <Styled.ButtonBox>
-        <Button borderColor="red" backgroundColor="white" color="black">
+        <Button
+          borderColor="red"
+          backgroundColor="white"
+          color="black"
+          onClick={() => onDeleteToDo(toDo.id)}
+        >
           Delete
         </Button>
         <Button borderColor="green" backgroundColor="white" color="black">
--- week2/src/modules/todos.js
+++ week2/src/modules/todos.js
@@ -11,6 +11,7 @@ const initialState = [
 
 //액션
 const ADD_TODO = 'todos/ADD_TODO';
+const DELETE_TODO = 'todos/DELETE_TODO';
 
 export const addNewToDo = (todo) => {
   const id = uuidv4();
@@ -21,11 +22,25 @@ export const addNewToDo = (todo) => {
   };
 };
 
+export const deleteToDo = (id) => ({
+  type: DELETE_TODO,
+  payload: id,
+});
+
 //리듀서
 export const toDos = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TODO:
       return [...state, action.payload];
+    case DELETE_TODO: {
+      const newState = [];
+      for (let i = 0; i < state.length; i++) {
+        if (state[i].id !== action.payload) {
+          newState.push(state[i]);
+        }
+      }
+      return newState;
+    }
     default:
       return state;
   }
