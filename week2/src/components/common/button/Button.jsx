74aa582664e82f3f6e61c76b1431acb446e4c195
yonainthefish <yonaisgood@gmail.com>
2024-07-06 16:50:20 +0900

    refactor : 버튼의 선색, 배경색, 넓이를 조절할 수 있도록 설정 값 수정

--- week2/src/components/common/button/Button.jsx
+++ week2/src/components/common/button/Button.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import { ButtonBox } from './StyledButton';
 
-export default function Button({ onClick, children }) {
-  return <ButtonBox onClick={onClick}>{children}</ButtonBox>;
+export default function Button({
+  onClick,
+  children,
+  bgColor,
+  color,
+  borderColor,
+  width,
+}) {
+  return (
+    <ButtonBox
+      onClick={onClick}
+      bgColor={bgColor}
+      color={color}
+      borderColor={borderColor}
+      width={width}
+    >
+      {children}
+    </ButtonBox>
+  );
 }
--- week2/src/components/common/button/StyledButton.jsx
+++ week2/src/components/common/button/StyledButton.jsx
@@ -1,14 +1,17 @@
 import styled from 'styled-components';
 
 const ButtonBox = styled.button`
-  border: none;
-  height: 40px;
-  cursor: pointer;
+  border: 1px solid ${(props) => props.borderColor || 'transparent'};
+  background-color: ${(props) => props.bgColor || 'teal'};
+  color: ${(props) => props.color || 'rgb(255, 255, 255)'};
+  width: ${(props) => props.width || '140px'};
+  padding: 10px;
   border-radius: 10px;
-  background-color: teal;
-  width: 140px;
-  color: rgb(255, 255, 255);
   font-weight: 700;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export { ButtonBox };
