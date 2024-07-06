a6bafea4a2e66f629431d300354a8234115e9847
yonainthefish <yonaisgood@gmail.com>
2024-07-07 02:10:18 +0900

    feat : MainPage 생성

---
+++ week2/src/pages/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/common/header/Header';
+import Form from '../components/common/form/Form';
+import Layout from '../components/layout/processLayOut/Layout';
+
+export default function MainPage() {
+  return (
+    <>
+      <Header />
+      <Form />
+      <Layout />
+    </>
+  );
+}
