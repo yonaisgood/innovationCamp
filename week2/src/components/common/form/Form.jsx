2a88b80ede9f54d634a8037254f4ab7b2e1e3417
yonainthefish <yonaisgood@gmail.com>
2024-07-06 23:55:11 +0900

    feat : Form 컴포넌트 상태변경 데이터 dispatch 전달 추가

--- week2/src/components/common/form/Form.jsx
+++ week2/src/components/common/form/Form.jsx
@@ -1,14 +1,50 @@
-// import React from 'react';
+import { useState } from 'react';
 import Input from '../input/Input';
 import Button from '../button/Button';
 import { FormBox } from './StyledForm';
+import { useDispatch } from 'react-redux';
+import { addNewToDo } from '../../../modules/todos';
 
 export default function Form() {
+  const [title, setTitle] = useState('');
+  const [contents, setContents] = useState('');
+  const dispatch = useDispatch();
+
+  const onSubmitNewToDo = (e) => {
+    e.preventDefault();
+    if (title === '' || contents === '') return;
+
+    dispatch(
+      addNewToDo({
+        title,
+        contents,
+        isDone: false,
+      }),
+    );
+
+    setTitle('');
+    setContents('');
+  };
+
   return (
-    <FormBox>
+    <FormBox onSubmit={onSubmitNewToDo}>
       <div>
-        <Input label="Title" type="text" name="title" id="Title" />
-        <Input label="Content" type="text" name="content" id="Content" />
+        <Input
+          id="Title"
+          label="Title"
+          type="text"
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <Input
+          label="Content"
+          type="text"
+          name="content"
+          id="Content"
+          value={contents}
+          onChange={(e) => setContents(e.target.value)}
+        />
       </div>
       <Button>Add</Button>
     </FormBox>
