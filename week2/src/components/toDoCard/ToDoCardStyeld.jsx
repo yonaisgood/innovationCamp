891613a0c31387f5eb905f4e311e379c37d9e9f5
yonainthefish <yonaisgood@gmail.com>
2024-07-06 17:38:13 +0900

    styled : TodoCard 컴포넌트 넓이 수정

--- week2/src/components/toDoCard/ToDoCardStyeld.jsx
+++ week2/src/components/toDoCard/ToDoCardStyeld.jsx
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 
 const TodoCardBox = styled.div`
-  width: 270px;
   border: 4px solid teal;
+  width: calc(100 - 33.33%);
+  max-width: 270px;
   min-height: 150px;
   border-radius: 12px;
   padding: 12px 24px 24px;
+  flex-grow: 1;
 
   & > Button:first-of-type {
     float: right;
