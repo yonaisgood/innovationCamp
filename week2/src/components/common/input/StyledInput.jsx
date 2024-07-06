591c3727ff0852f8698f19fd1579c4400cfc1317
yonainthefish <yonaisgood@gmail.com>
2024-07-06 15:17:53 +0900

    style : Input 공통 컴포넌트 스타일링 추가

--- week2/src/components/common/input/StyledInput.jsx
+++ week2/src/components/common/input/StyledInput.jsx
@@ -1,7 +1,18 @@
 import styled from 'styled-components';
 
 const InputContainer = styled.div`
-  border: 1px solid gray;
+  label {
+    font-size: 16px;
+    font-weight: 700;
+  }
+
+  input {
+    height: 40px;
+    width: 240px;
+    border: none;
+    border-radius: 12px;
+    padding: 0px 12px;
+  }
 `;
 
 export { InputContainer };
