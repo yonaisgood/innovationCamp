b8286530e55ed8ffefcd3bb423e69411ed9207b4
yonainthefish <yonaisgood@gmail.com>
2024-07-06 14:45:07 +0900

    feat : Header 공통 컴포넌트 생성

---
+++ week2/src/components/common/header/Header.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+import { HeaderContainer } from './StyledHeader';
+
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <p>My Todo List</p>
+      <p>React</p>
+    </HeaderContainer>
+  );
+}
---
+++ week2/src/components/common/header/StyledHeader.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border: 1px solid gray;
+  padding: 10px 20px;
+`;
