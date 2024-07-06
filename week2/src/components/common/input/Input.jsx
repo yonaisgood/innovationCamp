0fed8aa880a435ced7d4ecc4e2a57896a7219d0b
yonainthefish <yonaisgood@gmail.com>
2024-07-06 18:48:21 +0900

    feat : Input 컴포넌트에 추가적으로 필요한 props 추가

--- week2/src/components/common/input/Input.jsx
+++ week2/src/components/common/input/Input.jsx
@@ -1,11 +1,17 @@
 // import React from 'react';
 import { InputContainer } from './StyledInput';
 
-export default function Input({ label, type, name, id }) {
+export default function Input({ label, type, name, id, onChange, value }) {
   return (
     <InputContainer>
       <label htmlFor={id}>{label}</label>
-      <input type={type} name={name} id={id} />
+      <input
+        type={type}
+        name={name}
+        id={id}
+        onChange={onChange}
+        value={value}
+      />
     </InputContainer>
   );
 }
