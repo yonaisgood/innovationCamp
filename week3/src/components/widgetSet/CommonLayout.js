feb983124efe43bfb0802c28fb560a497669d84b
yonainthefish <yonaisgood@gmail.com>
2024-07-10 19:12:15 +0900

    style : 버튼과 인풋 컴포넌트 모두 적용되는 레이아웃으로 스타일 수정

--- week3/src/components/widgetSet/CommonLayout.js
+++ week3/src/components/widgetSet/CommonLayout.js
@@ -2,18 +2,22 @@ import styled from 'styled-components';
 
 const SectionLayout = styled.section`
   width: 100%;
-  border: 1px solid red;
+
+  .BtnRow {
+    display: flex;
+    gap: 10px;
+  }
+
+  .FormBox {
+    display: flex;
+    gap: 30px;
+  }
 `;
 
 const LayoutContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 10px;
-
-  .BtnRow {
-    display: flex;
-    gap: 10px;
-  }
 `;
 
 export { SectionLayout, LayoutContainer };
